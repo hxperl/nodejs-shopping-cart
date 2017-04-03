247313e4089aaafc23258cf8f78608555133eb9a
hxperl <hxperl@gmail.com>
2017-04-03 15:44:36 -0400

    Sign up with Passport

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 var Product = require('../models/product')
 var csrf = require('csurf');
+var passport = require('passport');
 
 var csrfProtection = csrf();
-router.use(csrfProtection)
+router.use(csrfProtection);
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Product.find(function(err, docs){
@@ -18,11 +19,18 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/user/signup', function(req, res, next){
-  res.render('user/signup', {csrfToken:req.csrfToken});
+  var messages = req.flash('error');
+  res.render('user/signup', {csrfToken:req.csrfToken, messages: messages, hasErrors:messages.length >0});
 });
 
-router.post('/user/signup', function(req, res, next){
-  res.redirect('/');
-});
+router.post('/user/signup', passport.authenticate('local.signup', {
+  successRedirect: '/user/profile',
+  failureRedirect: '/user/signup',
+  failureFlash: true
+}));
+
+router.get('/user/profile', function(req, res, next){
+  res.render('user/profile');
+})
 
 module.exports = router;
