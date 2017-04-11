c828ce2f034ca19e7251caa8c8eb8fc8d38f129d
hxperl <hxperl@gmail.com>
2017-04-11 00:11:28 -0400

    Forcing User Login

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
 var validator = require('express-validator');
+var MongoStroe = require('connect-mongo')(session);
 
 
 var app = express();
@@ -31,7 +32,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(validator());
 app.use(cookieParser());
-app.use(session({secret:'mysupersecret', resave: false, saveUninitialized: false}));
+app.use(session({
+  secret:'mysupersecret', 
+  resave: false, 
+  saveUninitialized: false,
+  store: new MongoStroe({ mongooseConnection: mongoose.connection }),
+  cookie: { maxAge: 180 * 60 * 1000 }
+}));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
@@ -39,6 +46,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next){
   res.locals.login=req.isAuthenticated();
+  res.locals.session = req.session;
   next();
 })
 
